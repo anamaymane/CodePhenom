aefc65ccff10d525b632c7532e75c53eba21953f
anamaymane <anamaymane@gmail.com>
2020-06-10 14:40:27 +0100

    add

--- CodePhenomRemastered/src/main/java/Dao/ProblemDao.java
+++ CodePhenomRemastered/src/main/java/Dao/ProblemDao.java
@@ -1,4 +1,38 @@
 package Dao;
 
+import DatabasePackage.HibernateOGMUtil;
+import Model.Problem;
+import Model.User;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+
+@ManagedBean(name = "problemDao")
+@ApplicationScoped
 public class ProblemDao {
+
+    private EntityManager entityManager;
+    private static EntityManagerFactory entityManagerFactory;
+
+    public int getProblemSize() throws ClassNotFoundException {
+
+        entityManagerFactory = HibernateOGMUtil.setUpEntityManagerFactory();
+
+        entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        String query = "FROM Problem as h";
+
+        List<Problem> problems = entityManager.createQuery( query , Problem.class )
+                .getResultList();
+
+        System.out.println("the size is : " + problems.size());
+
+        return problems.size();
+    }
 }
